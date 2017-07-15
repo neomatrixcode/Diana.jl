f5df6478018ed41dbc15735fa6fe6d17f07d191d
neomatrix <josuecevedo@gmail.com>
2017-07-14 23:26:32 -0500

    Update Diana

--- src/Diana.jl
+++ src/Diana.jl
@@ -6,19 +6,9 @@ import Requests: post
 export query
 
 function query(url::String,data::String)
-
-  r=post(url; json = Dict("query"=>data),headers = Dict("Accept" => "application/json"))
-  
-  r_data = r.data 
-  length_data=length(r_data)
-
+  r=post(url; json = Dict("query"=>data),headers = Dict("Accept" => "application/json"))  
   content=""
-  if(length_data>=1)
-    for i=1:length_data
-      content*="$(Char(r_data[i]))"
-    end
-  end
-
+  map(x -> (content*="$(Char(x))"), r.data)
   content
 end
 
