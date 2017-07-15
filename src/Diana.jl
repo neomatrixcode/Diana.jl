716d5563b61f27679e9736b34984575326fe0117
codeneomatrix <josuecevedo@gmail.com>
2017-07-14 22:27:16 -0500

    first query

--- src/Diana.jl
+++ src/Diana.jl
@@ -1,5 +1,26 @@
 module Diana
 
-# package code goes here
+using Requests
+import Requests: post
+
+export query
+
+function query(url::String,data::String)
+
+  r=post(url; json = Dict("query"=>data),headers = Dict("Accept" => "application/json"))
+  
+  r_data = r.data 
+  length_data=length(r_data)
+
+  content=""
+  if(length_data>=1)
+    for i=1:length_data
+      content*="$(Char(r_data[i]))"
+    end
+  end
+
+  content
+end
+
 
 end # module
