ee6251c56136080bcad466e2a76d14c2175f646a
Rashaine Johnson <rasjoh@RAJMBP.local>
2018-06-27 10:05:07 -0400

    Provide optional headers when making graphql query

--- src/client.jl
+++ src/client.jl
@@ -9,14 +9,14 @@ mutable struct Result
 	Data::String
 end
 
-function Query(url::String,data::String; vars::Dict=Dict(),auth::String="Bearer 0000")
-  r=post(url; json = Dict("query"=>data,"variables" => vars),headers = Dict("Accept" => "application/json","Content-Type" => "application/json" ,"Authorization" => auth))  
+function Query(url::String,data::String; vars::Dict=Dict(),auth::String="Bearer 0000", headers::Dict=Dict())
+  r=post(url; json = Dict("query"=>data,"variables" => vars),headers = merge(Dict("Accept" => "application/json","Content-Type" => "application/json" ,"Authorization" => auth), headers))
   content=""
   r.status == 200 ? map(x -> (content*="$(Char(x))"), r.data): content="{\"data\":{}}"
   return Result(r,content)
 end
 
-function GraphQLClient(url::String,auth::String="Bearer 0000")
+function GraphQLClient(url::String, auth::String="Bearer 0000", headers::Dict=Dict())
 
 my_url::String= url
 my_auth::String= auth
@@ -30,7 +30,7 @@ my_auth::String= auth
 	end
 
 	function Query(data::String;vars::Dict=Dict())
-	  r=post(my_url; json = Dict("query"=>data,"variables" => vars),headers = Dict("Accept" => "application/json","Content-Type" => "application/json" ,"Authorization" => my_auth))  
+	  r=post(my_url; json = Dict("query"=>data,"variables" => vars),headers = merge(Dict("Accept" => "application/json","Content-Type" => "application/json" ,"Authorization" => my_auth), headers))
 	  content=""
 	  r.status == 200 ? map(x -> (content*="$(Char(x))"), r.data): content="{\"data\":{}}"
 	  return Result(r,content)
