fec84d3e142b92410cbac1103d5d3ef041dd932b
codeneomatrix <josuecevedo@gmail.com>
2018-08-07 20:33:18 -0500

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,49 +1,8 @@
 using Diana
 using Test
-include("validationast.jl")
 
-query = """
-{
-  neomatrix{
-    nombre
-    linkedin
-  }
-}
-"""
+@testset "Diana" begin
+include("cliente.jl");
+include("validationast.jl");
+end;
 
-r = Queryclient("https://neomatrix.herokuapp.com/graphql",query)
-
-@test r.Info.status == 200
-@test r.Data == "{\"data\":{\"neomatrix\":{\"nombre\":\"Acevedo Maldonado Josue\",\"linkedin\":\"https://www.linkedin.com/in/acevedo-maldonado-josue/\"}}}"
-
-
-client = GraphQLClient("https://api.graph.cool/simple/v1/movies","Bearer my-jwt-token")
-
-query2 = """
-query getMovie(\$title: String!) {
-  Movie(title:\$title) {
-    releaseDate
-    actors {
-      name
-    }
-  }
-}
-"""
-r = client.Queryclient(query2,vars=Dict("title" => "Inception"))
-
-@test r.Info.status == 200
-@test r.Data == "{\"data\":{\"Movie\":{\"releaseDate\":\"2010-08-28T20:00:00.000Z\",\"actors\":[{\"name\":\"Leonardo DiCaprio\"},{\"name\":\"Ellen Page\"},{\"name\":\"Tom Hardy\"},{\"name\":\"Joseph Gordon-Levitt\"},{\"name\":\"Marion Cotillard\"}]}}}"
-
-query2 = """
-{
-  Movie(title: "Inception"){
-    actors{
-      name
-    }
-  }
-}
-"""
-r = client.Queryclient(query2)
-
-@test r.Info.status == 200
-@test r.Data == "{\"data\":{\"Movie\":{\"actors\":[{\"name\":\"Leonardo DiCaprio\"},{\"name\":\"Ellen Page\"},{\"name\":\"Tom Hardy\"},{\"name\":\"Joseph Gordon-Levitt\"},{\"name\":\"Marion Cotillard\"}]}}}"
