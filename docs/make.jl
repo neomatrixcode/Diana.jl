08fa301d50c659c91ea0d3f8cc2313ce97ae294b
codeneomatrix <josuecevedo@gmail.com>
2019-08-17 22:42:56 -0500

    update documentation

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,8 @@ makedocs(format = Documenter.HTML(),
          sitename = "Diana.jl"
          )
 deploydocs(
-	repo = "github.com/codeneomatrix/Diana.jl.git",
-	devbranch = "master",
-    devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", "devurl" => devurl]
+	repo = "github.com/codeneomatrix/Diana.jl.git"
+	#devbranch = "master",
+    #devurl = "dev",
+    #versions = ["stable" => "v^", "v#.#", "devurl" => devurl]
     )
